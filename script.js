50df2b139b986d9477076430a538658f94275aca
martonkertesz <kertesz.marton1992@gmail.com>
2023-10-25 17:50:11 +0200

    arr-for

---
+++ script.js
@@ -0,0 +1,40 @@
+/* let variable = 10
+variable = 20
+
+const variable2 = 50
+// variable2 = 100 <-- hibara fut, mert const valtozot nem lehet = jellel uj erteket adni
+
+let array = [1, 2, 3]
+array = [4, 5, 6]
+array.push(10)
+
+const array2 = [10, 20, 30]
+// array2 = [40, 50, 60]
+array2.push(100)
+array2[0] = 11
+
+console.log(array2)
+ */
+
+
+
+const arr = ["kiss bela", "nagy jozsi", "toth janos"]
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+ */
+
+lett lettersLength = o
+arr.forEachResult = arr.forEach((value, index) => {
+    //console.log("at index: ", index, "is", value)
+    console.log(`at index: ${index + 1} is: ${value}`) // template literal, ${jsExpression} <- template literal expression
+    return v
+})
+
+let mapResult = arr.map((value, index) => {
+    console.log(`at index: ${index + 1} is: ${value}`)
+})
+
+console.log("letterslengthh:", lettersLength)
+console.log("mapResult, ")
